7ab8305b441d13c3fb08ba52137d0ce3ab4427da
Don Livanec <donlivanec@gmail.com>
2011-04-18 05:29:55 -0400

    ch 8 fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,19 +39,18 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.xml
+  end
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
     end
   end
+  
 
   # PUT /users/1
   # PUT /users/1.xml
